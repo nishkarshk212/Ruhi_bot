bde82f3f09ceb746aaccbd2fd821a5622bd9dfbd
Nishkarsh Sharma <nishkarshsharma@Nishkarshs-MacBook-Air.local>
2026-04-18 15:43:25 +0530

    fix: InlineKeyboardButton user_id to url in private_panel

--- ANNIEMUSIC/utils/inline/start.py
+++ ANNIEMUSIC/utils/inline/start.py
@@ -25,7 +25,7 @@ def private_panel(_):
             )
         ],
         [
-            InlineKeyboardButton(text=_["S_B_10"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_10"], url=f"tg://user?id={config.OWNER_ID}"),
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHAT),
         ],
         [
