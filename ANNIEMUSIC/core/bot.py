fa31916095853e6e80e3e8a34723cd98d3ae8b1a
Nishkarsh Sharma <nishkarshsharma@Nishkarshs-MacBook-Air.local>
2026-04-18 16:16:05 +0530

    fix: change session name

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -11,7 +11,7 @@ class JARVIS(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="ANNIEMUSIC",
+            name="ANNIEMUSIC_NEW",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
